ba641425c77c1b6a34f4b3a78285a919f1041c9e
Sunny <77861511+someoneelsescloud@users.noreply.github.com>
2021-08-10 15:26:26 +1000

    Update to Az Module

--- scripts/Create-TFServicePrincipal/Create-TFServicePrincipal.ps1
+++ scripts/Create-TFServicePrincipal/Create-TFServicePrincipal.ps1
@@ -1,30 +1,40 @@
 # Initialize
 $ErrorActionPreference = "Stop"
+$WarningPreference = "SilentlyContinue"
 $VerbosePreference = "SilentlyContinue"
 $userName = $env:USERNAME
 
-# Service Principal Role
-$spnRole = "contributor"
-
 # Initialize subscription
-$isAzureModulePresent = Get-Module -Name AzureRM* -ListAvailable
-if ([String]::IsNullOrEmpty($isAzureModulePresent) -eq $true) {
-    Write-Output "Script requires AzureRM modules. Obtain from https://github.com/Azure/azure-powershell/releases."
+$isAzModulePresent = Get-Module -Name Az.* -ListAvailable
+if ([String]::IsNullOrEmpty($isAzModulePresent) -eq $true) {
+    Write-Output "Script requires Az modules. Obtain from https://github.com/Azure/azure-powershell."
     return
 }
+else {
+    Write-Output "Importing Az module.."
+    Import-Module Az
+}
 
-Import-Module -Name AzureRM.Profile
-Write-Output "Provide your credentials to access your Azure subscription - See Login Pop-up Window"
-Connect-AzureRmAccount
+# Checking if connected to Azure
+if (!(Get-AzContext)) {
+    try {
+        Write-Output "Connecting to Azure."
+        Write-Output "Provide your credentials to access your Azure subscription - See Login Pop-up Window"
+        Connect-AzAccount
+    }
+    catch {
+        Write-Output "Error: Cannot connect to Azure!"
+    }
+}
 
-# Collect AzureRM Subscription details to create Service principal
-$azureAccount = Get-AzureRmSubscription
-$tenantId = $azureAccount.TenantId
-$subscriptionId = $azureAccount.Id
+# Collect AzSubscription details to create Service principal
+$azAccount = Get-AzSubscription
+$tenantId = $azAccount.TenantId
+$subscriptionId = $azAccount.Id
 
 # Generate random suffix for name
 $appRandom = ("0123456789".tochararray() | sort {Get-Random})[0..4] -join ''
-$appDisplayName = "terraform-spn-$appRandom"
+$appDisplayName = "terraform-sp-$appRandom"
 
 # Service Principal URI
 $homePage = "http://$tenantId/$appDisplayName"
@@ -35,7 +45,7 @@ $appPassword = ("!@#$%^&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqr
 
 # Check if AD Application Identifier URI is unique
 Write-Output "Verifying App URI is unique ($identifierUri)"
-$existingApplication = Get-AzureRmADApplication -IdentifierUri $identifierUri
+$existingApplication = Get-AzADApplication -IdentifierUri $identifierUri
 if ($existingApplication -ne $null) {
     $appId = $existingApplication.ApplicationId
     Write-Output "An AAD Application already exists with App URI $identifierUri (Application Id: $appId). Choose a different app display name" 
@@ -45,35 +55,26 @@ if ($existingApplication -ne $null) {
 # Create a new AD Application
 Write-Output "Creating a new Application in AAD (App URI - $identifierUri)"
 $secureAppPassword = $appPassword | ConvertTo-SecureString -AsPlainText -Force
-$azureAdApplication = New-AzureRmADApplication -DisplayName $appDisplayName -HomePage $homePage -IdentifierUris $identifierUri -Password $secureAppPassword
+$azureAdApplication = New-AzADApplication -DisplayName $appDisplayName -HomePage $homePage -IdentifierUris $identifierUri -Password $secureAppPassword
 $appId = $azureAdApplication.ApplicationId
 Write-Output "Azure AAD Application creation completed successfully (Application Id: $appId)"
 
 # Create new SPN
 Write-Output "Creating a new SPN"
-$spn = New-AzureRmADServicePrincipal -ApplicationId $appId
-$spnName = $spn.ServicePrincipalName
+$spn = New-AzADServicePrincipal -ApplicationId $appId
+$spnName = $spn.DisplayName
 Write-Output "SPN creation completed successfully (SPN Name: $spnName)"
 
-# Assign role to SPN
-Write-Output "Waiting for SPN creation to reflect in Directory before Role assignment"
-Start-Sleep 15
-Write-Output "Assigning role ($spnRole) to SPN App ($appId)"
-New-AzureRmRoleAssignment -RoleDefinitionName $spnRole -ServicePrincipalName $appId
-Write-Output "SPN role assignment completed successfully"
-
 # Print the values
-Write-Output "Copy and Paste below values for Terraform AzureRM Provider in providers.tf"
-Write-Output "--------------------------------------------------------------------------"
-Write-Output "provider ""azurerm"" {"
-Write-Output "`n"
-Write-Output "subscription_id = ""$subscriptionId"""
-Write-Output "client_id = ""$appId"""
-Write-Output "client_secret = ""$appPassword"""
-Write-Output "tenant_id = ""$tenantId"""
-Write-Output "`n"
-Write-Output "features {}"
-Write-Output "}"
-Write-Output "--------------------------------------------------------------------------"
+Write-Output "Creating providers.tf configuration file for Terraform AzureRM Provider authentication."
+Set-Content "providers.tf" "provider ""azurerm"" {"
+Add-Content "providers.tf" "`n"
+Add-Content "providers.tf" "subscription_id = ""$subscriptionId"""
+Add-Content "providers.tf" "client_id = ""$appId"""
+Add-Content "providers.tf" "client_secret = ""$appPassword"""
+Add-Content "providers.tf" "tenant_id = ""$tenantId"""
+Add-Content "providers.tf" "`n"
+Add-Content "providers.tf" "features {}"
+Add-Content "providers.tf" "}"
 Write-Output "** appPassword saved to $appDisplayName.txt **"
 $appPassword | Out-File "$appDisplayName.txt"
